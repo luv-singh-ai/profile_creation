3e495f2f485f7ee8a9200412e9e38e1e34aa2ce8
LUV SINGH <luv13singh@gmail.com>
2024-04-01 12:55:05 +0530

    added text capability

---
+++ main.py
@@ -0,0 +1,249 @@
+import os
+import base64
+import tempfile
+import time
+from typing import Union
+
+import asyncio
+import logging
+import dotenv
+
+from telegram import (
+    Update, 
+    InlineKeyboardButton, 
+    InlineKeyboardMarkup
+)
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    MessageHandler,
+    CommandHandler,
+    filters,
+    CallbackContext,
+    CallbackQueryHandler,
+)
+
+from core.ai import (
+    chat, 
+    audio_chat, 
+    bhashini_text_chat, 
+    bhashini_audio_chat
+)
+from utils.redis_utils import set_redis
+from utils.openai_utils import (
+    get_duration_pydub, 
+    get_random_wait_messages
+)
+
+dotenv.load_dotenv("ops/.env")
+
+token = os.getenv('TELEGRAM_BOT_TOKEN')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+class BotInitializer:
+    _instance = None
+    run_once = False
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(BotInitializer, cls).__new__(cls)
+            cls.run_once = True
+        return cls._instance
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    BotInitializer()  # To initialize only once
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text="Hello I am Yojana Didi , please share your details with me."
+        # func to take consent from user to be added later
+    )
+    await relay_handler(update, context)
+
+async def relay_handler(update: Update, context: CallbackContext):
+    await language_handler(update, context)
+    
+async def language_handler(update: Update, context: CallbackContext):
+    # Handle user's language selection
+    keyboard = [
+        [InlineKeyboardButton("English", callback_data='1')],
+        [InlineKeyboardButton("हिंदी", callback_data='2')],
+        #[InlineKeyboardButton("ਪੰਜਾਬੀ", callback_data='3')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text="Choose a Language:", 
+        reply_markup=reply_markup
+    )
+
+async def preferred_language_callback(update: Update, context: CallbackContext):
+    
+    callback_query = update.callback_query
+    languages = {"1": "en", "2": "hi"} # "3": "pa"
+    try:
+        preferred_language = callback_query.data
+        lang = languages.get(preferred_language)
+        context.user_data['lang'] = lang
+    except (AttributeError, ValueError):
+        lang = 'en'
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, 
+            text="Error getting language! Setting default to English."
+        )
+    
+    text_message = ""
+    if lang == "en":
+        text_message = "You have chosen English. \nPlease share your details"
+    else:
+        text_message = "आपने हिंदी चुनी है. \nकृपया मुझे अपने बारे में बताएं।"
+    # elif lang == "pa":
+    #     text_message = "ਤੁਸੀਂ ਪੰਜਾਬੀ ਨੂੰ ਚੁਣਿਆ ਹੈ। \ਕਿਰਪਾ ਕਰਕੇ ਹੁਣੇ ਆਪਣੀ ਸ਼ਿਕਾਇਤ ਦਿਓ"
+        
+    set_redis('lang', lang)
+    
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=text_message
+    )
+
+async def response_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await query_handler(update, context)
+
+def check_change_language_query(text):
+    return text.lower() in ["change language", "set language", "language"]
+
+async def query_handler(update: Update, context: CallbackContext):
+
+    lang = context.user_data.get('lang')
+    if not lang:
+        await language_handler(update, context)
+        return
+
+    if update.message.text:
+        text = update.message.text
+        print(f"text is {text}")
+        if check_change_language_query(text):
+            await language_handler(update, context)
+            return
+        await chat_handler(update, context, text)
+    elif update.message.voice:
+        voice = await context.bot.get_file(update.message.voice.file_id)
+        await talk_handler(update, context, voice)
+
+async def chat_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, text: str):
+    response = ""
+    chat_id = update.effective_chat.id
+    lang = context.user_data.get('lang')
+    wait_message = get_random_wait_messages(
+        not_always=True,
+        lang=lang
+    )
+    if wait_message:
+        await context.bot.send_message(chat_id=chat_id, text=wait_message)
+    if lang == 'en':
+        response_en, history = chat(chat_id, text)
+    else:
+        response, response_en, history = bhashini_text_chat(chat_id,text, lang)
+    if response:
+        await context.bot.send_message(chat_id=chat_id, text=response)
+    await context.bot.send_message(chat_id=chat_id, text=response_en)
+
+async def talk_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, voice):    
+    lang = context.user_data.get('lang')
+    # getting audio file
+    audio_file = voice
+
+    if lang == 'en':
+        with tempfile.NamedTemporaryFile(suffix='.wav', delete=True) as temp_audio_file:
+            await audio_file.download_to_drive(custom_path=temp_audio_file.name)
+            chat_id = update.effective_chat.id
+
+            wait_message = get_random_wait_messages(
+                not_always=True,
+                lang=lang
+        )
+            if wait_message:
+                await context.bot.send_message(chat_id=chat_id, text=wait_message)
+
+            with open(temp_audio_file.name, "rb") as file:
+                audio_data = file.read()
+                audio_base64 = base64.b64encode(audio_data).decode('utf-8')
+
+        
+                response_audio, assistant_message, history = audio_chat(
+                    chat_id, audio_file=open(temp_audio_file.name, "rb")
+                )
+                response_audio.stream_to_file(temp_audio_file.name)
+                duration = get_duration_pydub(temp_audio_file.name)
+                await context.bot.send_audio(
+                    chat_id=chat_id, 
+                    audio=open(temp_audio_file.name, "rb"), 
+                    duration=duration, 
+                    filename="response.wav",
+                    performer="Yojana Didi",
+                )
+                await context.bot.send_message(
+                    chat_id=chat_id, text=assistant_message
+                )
+                file.close()
+    else:
+        with tempfile.NamedTemporaryFile(suffix='.mp3', delete=True) as temp_audio_file:
+            await audio_file.download_to_drive(custom_path=temp_audio_file.name)
+            chat_id = update.effective_chat.id
+
+            wait_message = get_random_wait_messages(
+                    not_always=True,
+                    lang=lang
+            )
+            if wait_message:
+                await context.bot.send_message(chat_id=chat_id, text=wait_message)
+
+            with open(temp_audio_file.name, "rb") as file:
+                audio_data = file.read()
+                audio_base64 = base64.b64encode(audio_data).decode('utf-8')
+                response_audio, response, history = bhashini_audio_chat(
+                    chat_id, 
+                    audio_file=audio_base64, 
+                    lang=lang
+                )
+                file_ = open(temp_audio_file.name, "wb")
+                file_.write(response_audio.content)
+                file_.close()
+                with open(temp_audio_file.name, "rb") as file:
+                    duration = get_duration_pydub(temp_audio_file.name)
+                    await context.bot.send_audio(
+                        chat_id=chat_id, 
+                        audio=open(temp_audio_file.name, "rb"), 
+                        duration=duration, 
+                        filename="response.mp3",
+                        performer="Yojana Didi",
+                    )
+                await context.bot.send_message(
+                    chat_id=chat_id, text=response
+                )
+                file_.close()
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(
+        token
+    ).read_timeout(30).write_timeout(30).build()
+    #start_handler = CommandHandler('start', start)
+    language_handler_ = CommandHandler('set_language', language_handler)
+    chosen_language = CallbackQueryHandler(preferred_language_callback, pattern='[1-3]')
+    #application.add_handler(start_handler)
+    application.add_handler(language_handler_)
+    application.add_handler(chosen_language)
+    application.add_handler(
+        MessageHandler(
+            (filters.TEXT & (~filters.COMMAND)) | (filters.VOICE & (~filters.COMMAND)), 
+            response_handler
+        )
+    )
+    application.run_polling()
+    
+
---
+++ utils/openai_utils.py
@@ -0,0 +1,296 @@
+from dotenv import load_dotenv
+from utils.bhashini_utils import bhashini_translate
+from utils.redis_utils import set_redis
+import random
+from pydub import AudioSegment
+import time
+import os
+
+
+load_dotenv(
+    dotenv_path="ops/.env",
+)
+
+with open("prompts/prompt.txt", "r") as file:
+    main_prompt = file.read().replace('\n', ' ')
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+assistant_id = os.getenv("ASSISTANT_ID")
+model_name = os.getenv("MODEL_NAME")
+
+#OPENAI FUNCTION CALLS
+'''
+{
+    "firstName": <value>,
+    "lastName": <value>,
+    "mobile": <value>,
+    "gender": <value>,
+    "maritalStatus": <value>,
+    "dob": <value>,
+}
+'''   
+
+get_user_details = {
+    "name": "get_user_details",
+    "description": "Get these user details namely, Person's Name, Mobile Number, Gender, Marital Status, Date of Birth",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "firstName": {
+                "type": "string",
+                "description": "First Name"
+            },
+            "lastName": {
+                "type": "string",
+                "description": "Last Name"
+            },
+            "mobile": {
+                "type": "string", # 'integer'
+                "description": "Mobile Number"
+            },
+            "gender":{
+                "type": "string",
+                "description": "gender where 'M' is for Male, 'F' is for Female, 'O' is for Others",
+                "enum": ["M", "F", "O"], # M for Male, F for Female, O for Other
+            },
+            "maritalStatus": {
+                "type": "string",
+                "description": "Marital Status",
+                "enum": ["Single", "Married", "Divorced", "Widowed", "Others"]
+            },
+            "dob": {
+                "type": "string",
+                "description": "Date of Birth"
+            }
+        },
+        "required": ["firstName", "LastName", "mobile", "gender", "maritalStatus", "dob"]
+    }
+}
+
+def create_assistant(client, assistant_id):
+    try:
+        assistant = client.beta.assistants.retrieve(assistant_id=assistant_id)
+        return assistant
+    except Exception as e:
+        assistant = client.beta.assistants.create(
+        name="AI Assistant",
+        instructions=main_prompt,
+        model=model_name,
+        tools=[
+                {
+                    "type": "function",
+                    "function": get_user_details # function_call = {'name': 'get_user_details'},
+                }
+                # {
+                #     "type": "retrieval",
+                #      instructions="You are a customer support chatbot. Use your knowledge base to best respond to customer queries.",
+                # }
+            ]
+        )
+        set_redis("assistant_id", assistant.id)
+        return assistant
+
+def create_thread(client):
+    thread = client.beta.threads.create()
+    return thread
+
+def create_run(client, thread_id, assistant_id):
+    run = client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id,
+    )
+    return run.id, run.status
+
+def run_with_streaming_responses(client, thread_id, assistant_id):
+    # to add streaming responses
+    from typing_extensions import override
+    from openai import AssistantEventHandler
+    # First, we create a EventHandler class to define
+    # how we want to handle the events in the response stream.
+    class EventHandler(AssistantEventHandler):    
+        @override
+        def on_text_created(self, text) -> None:
+            print(f"\nassistant > ", end="", flush=True)
+            
+        @override
+        def on_text_delta(self, delta, snapshot):
+            print(delta.value, end="", flush=True)
+        
+        def on_tool_call_created(self, tool_call):
+            print(f"\nassistant > {tool_call.type}\n", flush=True)
+        
+        def on_tool_call_delta(self, delta, snapshot):
+            if delta.type == 'code_interpreter':
+                if delta.code_interpreter.input:
+                    print(delta.code_interpreter.input, end="", flush=True)
+            if delta.code_interpreter.outputs:
+                print(f"\n\noutput >", flush=True)
+                for output in delta.code_interpreter.outputs:
+                    if output.type == "logs":
+                        print(f"\n{output.logs}", flush=True)
+    # Then, we use the `create_and_stream` SDK helper 
+    # with the `EventHandler` class to create the Run 
+    # and stream the response.
+    with client.beta.threads.runs.create_and_stream(
+        thread_id=thread_id,
+        assistant_id=assistant_id,
+        instructions="Please address the user as Jane Doe. The user has a premium account.",
+        event_handler=EventHandler(),
+    ) as stream:
+        stream.until_done()
+
+def upload_message(client, thread_id, input_message, assistant_id):
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=input_message
+    )
+    run_id, run_status = create_run(client, thread_id, assistant_id)
+    return run_id, run_status
+
+def get_run_status(client, thread_id, run_id):
+    run = client.beta.threads.runs.retrieve(
+        thread_id=thread_id,
+        run_id=run_id
+    )
+    delay = 5
+    try: 
+        run_status = run.status
+        print(f"run status inside method is {run_status}")
+    except Exception as e:
+        return None, None
+
+    while run_status not in ["completed", "failed", "requires_action"]:
+        time.sleep(delay)
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id,
+        )
+        run_id = run.id
+        run_status = run.status
+        delay = 8 if run_status == "requires_action" else 5
+
+    return run_id, run_status
+
+def get_tools_to_call(client, thread_id, run_id):
+    run = client.beta.threads.runs.retrieve(
+        run_id=run_id,
+        thread_id=thread_id
+    )
+    tools_to_call = run.required_action.submit_tool_outputs.tool_calls ###
+    return tools_to_call, run.id, run.status
+
+def get_assistant_message(client, thread_id):
+    messages = client.beta.threads.messages.list(
+        thread_id=thread_id,
+    )
+    return messages.data[0].content[0].text.value
+
+
+def transcribe_audio(audio_file, client):
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1", 
+        file=audio_file
+    )
+    return transcript.text
+
+def generate_audio(text, client):
+    response = client.audio.speech.create(
+                model="tts-1",
+                voice="alloy",
+                input=text
+            )
+    return response
+
+def get_duration_pydub(file_path):
+   audio_file = AudioSegment.from_file(file_path)
+   duration = audio_file.duration_seconds
+   return duration
+
+def get_random_wait_messages(not_always=False, lang="en"):
+    messages = [
+        "Please wait",
+        "I am processing your request",
+        "Hold on",
+        "I am on it",
+        "I am working on it",
+    ]
+    if not_always:
+        rand = random.randint(0, 2)
+        if rand == 1:
+            random_message = random.choice(messages)
+            random_message = bhashini_translate(random_message, "en", lang)
+        else:
+            random_message = ""
+    else:
+        random_message = random.choice(messages)
+        random_message = bhashini_translate(random_message, "en", lang)
+    return random_message
+
+# raise_complaint ={
+#     "name": "raise_complaint",
+#     "description": "Raise complaint",
+#     "parameters": {
+#         "type": "object",
+#         "properties": {
+#             "description": {
+#                 "type": "string",
+#                 "description": "Detailed description of complaint"
+#             },
+#             "service_code": {
+#                 "type": "string",
+#                 "description": "service code of complaint extracted from description",
+#                 "enum": [
+#                     "GarbageNeedsTobeCleared", "NoStreetLight", "StreetLightNotWorking",
+#                     "BurningOfGarbage", "OverflowingOrBlockedDrain", "illegalDischargeOfSewage",
+#                     "BlockOrOverflowingSewage", "ShortageOfWater", "DirtyWaterSupply", "BrokenWaterPipeOrLeakage",
+#                     "WaterPressureisVeryLess", "HowToPayPT", "WrongCalculationPT", "ReceiptNotGenerated",
+#                     "DamagedRoad", "WaterLoggedRoad", "ManholeCoverMissingOrDamaged", "DamagedOrBlockedFootpath",
+#                     "ConstructionMaterialLyingOntheRoad", "RequestSprayingOrFoggingOperation", "StrayAnimals", "DeadAnimals",
+#                     "DirtyOrSmellyPublicToilets", "PublicToiletIsDamaged", "NoWaterOrElectricityinPublicToilet", "IllegalShopsOnFootPath",
+#                     "IllegalConstructions", "IllegalParking"
+#                 ]
+#             },
+#             "auth_token": {
+#                 "type": "string",
+#                 "description": "Authentication token of user"
+#             },
+#             "city": {
+#                 "type": "string",
+#                 "description": "City of the complaint"
+#             },
+#             "state": {
+#                 "type": "string",
+#                 "description": "State of the complaint"
+#             },
+#             "district": {
+#                 "type": "string",
+#                 "description": "district of the complaint"
+#             },
+#             "region": {
+#                 "type": "string",
+#                 "description": "region of the complaint"
+#             },
+#             "locality": {
+#                 "type": "string",
+#                 "description": "locality of the complaint"
+#             },
+#             "name": {
+#                 "type": "string",
+#                 "description": "name of the user"
+#             },
+#             "mobile_number": {
+#                 "type": "string",
+#                 "description": "mobile number of the user"
+#             },
+#         },
+#         "required": [
+#             "description",
+#             "service_code",
+#             "locality",
+#             "city",
+#             "name",
+#             "mobile_number"
+#         ]
+#     },
+# }
